6f5404d8061ad11d8048acea61050b9fcdbe31e2
programmerjalanan <andrianlatif001@gmail.com>
2018-09-16 23:54:05 -0400

    fixed file download 2

--- src/download/handler.go
+++ src/download/handler.go
@@ -3,5 +3,5 @@ package download
 import "github.com/labstack/echo"
 
 func GetApk(c echo.Context) (e error) {
-	return c.File("/home/ubuntu/go/src/retrobarbershop.com/retro/api/download/gps-1.0.apk")
+	return c.File("/home/ubuntu/go/src/gps.com/gps/download/gps-1.0.apk")
 }
